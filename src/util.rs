2c95a376f46780b754ddcbc34f6caa311db08646
eutropius225 <eutropius225@users.noreply.github.com>
2021-01-18 14:35:47 +0000

    Day 18 Part 1

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Sub};
+
 pub static DIRECTIONS: [Dir; 4] = [Dir::North, Dir::South, Dir::West, Dir::East];
 
 #[derive(Clone, Copy)]
@@ -9,12 +11,21 @@ pub enum Dir {
 }
 
 impl Dir {
-    pub fn offset(&self, (x, y): (i32, i32)) -> (i32, i32) {
+    pub fn offset<T: From<u8> + Add<Output=T> + Sub<Output=T>>(&self, (x, y): (T, T)) -> (T, T) {
         match self {
-            Dir::North => (x, y + 1),
-            Dir::South => (x, y - 1),
-            Dir::West => (x - 1, y),
-            Dir::East => (x + 1, y),
+            Dir::North => (x, y + 1.into()),
+            Dir::South => (x, y - 1.into()),
+            Dir::West => (x - 1.into(), y),
+            Dir::East => (x + 1.into(), y),
+        }
+    }
+
+    pub fn turns(&self) -> [Dir; 2] {
+        match self {
+            Dir::North => [Dir::West, Dir::East],
+            Dir::South => [Dir::East, Dir::West],
+            Dir::West => [Dir::North, Dir::South],
+            Dir::East => [Dir::South, Dir::North],
         }
     }
 }
